5c9f468a08487a45c7d2d8702a4acf5ee577d8a4
oligou <oliviergouvert@gmail.com>
2026-03-27 10:59:52 +0100

    Fix indentation

--- nemo_rl/data/llm_message_utils.py
+++ nemo_rl/data/llm_message_utils.py
@@ -154,8 +154,10 @@ def add_loss_mask_to_message_log(
         roles_to_train_on[i] = role.lower()
 
     for message_log in batch_message_log:
+        print("\n\n#### DEBUG token ids")
         for i, message in enumerate(message_log):
-            print("#### DEBUG token ids")
+            print(f"#### Turn {i}")
+            print(message["role"])
             print(message["token_ids"])
             if only_unmask_final:
                 if i == len(message_log) - 1:
@@ -655,8 +657,8 @@ def get_formatted_message_log(
                 else:
                     new_message["content"].append(item)
 
-    new_message_log.append(new_message)
-    prev_formatted_message = formatted_message
+        new_message_log.append(new_message)
+        prev_formatted_message = formatted_message
 
     return new_message_log
 
